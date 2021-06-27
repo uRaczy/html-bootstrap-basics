0fb3c218bf6e44f17568e9b76717f21fb7ca3c07
uRaczy <raczy7@gmail.com>
2021-06-27 02:00:12 +0200

    Rename classes to be shorter. Add hamburger menu bars change on click to X

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-const hamburgerMenu = document.querySelector('.header-nav-hamburger');
-const headerNavMobile = document.querySelector('.header-nav-mobile');
+const hamburgerMenu = document.querySelector('.nav-hamburger');
+const navMobile = document.querySelector('.nav-mobile');
+
+const navBars = document.querySelector('.nav-bars');
+const navX = document.querySelector('.nav-x');
 
 hamburgerMenu.addEventListener('click', () => {
-  if (headerNavMobile.style.display === "flex") {
-    headerNavMobile.style.display = "none";
+  if (navMobile.style.display === "flex") {
+    navMobile.style.display = "none";
+    navX.style.display = 'none';
+    navBars.style.display = 'block';
   } else {
-    headerNavMobile.style.display = "flex";
+    navMobile.style.display = "flex";
+    navX.style.display = 'block';
+    navBars.style.display = 'none';
   }
 });
 
 const setup = () => {
-  headerNavMobile.style.display = "none";
+  navMobile.style.display = "none";
+  navX.style.display = "none";
 }
 
 document.addEventListener('load', setup());
